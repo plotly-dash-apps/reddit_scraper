af200f93c76ff21f75766e55185eedeedb397a9a
Allen Chua <allenchua@Allens-MacBook-Pro.local>
2022-01-18 23:01:58 -0800

    Deploying HW9 update

--- helpers.orig.py
+++ helpers.orig.py
@@ -3,14 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
-import lxml
-import urllib.request
+
+
 
 ########### Set up the default figures ######
 
 def base_fig():
-    data=go.Table(columnwidth = [200,200,500,800],
-                    header=dict(values=['date', 'time', 'post', 'links'], align=['left']),
+    data=go.Table(columnwidth = [200,200,1000],
+                    header=dict(values=['date', 'time', 'post'], align=['left']),
                     cells=dict(align=['left'],
                                values=[[1,2,3],
                                        [1,2,3],
@@ -20,8 +20,8 @@ def base_fig():
     return fig
 
 def error_fig():
-    data=go.Table(columnwidth = [200,200,500,800],
-                    header=dict(values=['date', 'time', 'post', 'links'], align=['left']),
+    data=go.Table(columnwidth = [200,200,1000],
+                    header=dict(values=['date', 'time', 'post'], align=['left']),
                     cells=dict(align=['left'],
                                values=[['whoa!','whoa!','whoa!'],
                                        [3,2,1],
@@ -67,18 +67,10 @@ def scrape_reddit():
     for date in dates:
         output = str(date).split('title="')[1].split('2020')[0]
         dateslist.append(output)
-    # create URL links
-    parser = 'html.parser'
-    resp = urllib.request.urlopen("https://old.reddit.com/r/AskReddit/")
-    soup = BeautifulSoup(resp, parser, from_encoding=resp.info().get_param('charset'))
-    links = soup.find_all('a', 'comments', href=True)
-    linkslist = []
-    for link in links:
-        linkslist.append(link['href'])
-    #    print(link['href'])
+
     ########### Pandas work ######
-    # convert the 3 lists into a pandas dataframe
-    df_dict={'date':dateslist, 'post':titleslist, 'links':linkslist}
+    # convert the two lists into a pandas dataframe
+    df_dict={'date':dateslist, 'post':titleslist}
     working_df = pd.DataFrame(df_dict)
     pd.set_option('display.max_colwidth', 200)
     working_df['date'] = working_df['date'].str.strip()
@@ -93,19 +85,18 @@ def scrape_reddit():
     # split into 2 date/time variables
     working_df['date']=working_df['UTC_date'].dt.date
     working_df['time']=working_df['UTC_date'].dt.time
-    final_df = working_df[['date', 'time', 'post', 'links']].copy()
+    final_df = working_df[['date', 'time', 'post']].copy()
 
 
 
     ########### Set up the figure ######
 
-    data=go.Table(columnwidth = [200,200,500,800],
+    data=go.Table(columnwidth = [200,200,1000],
                     header=dict(values=final_df.columns, align=['left']),
                     cells=dict(align=['left'],
                                values=[final_df['date'],
                                        final_df['time'],
-                                       final_df['post'].values
-                                       final_df['links'].values])
+                                       final_df['post'].values])
                  )
     fig = go.Figure([data])
     return fig
